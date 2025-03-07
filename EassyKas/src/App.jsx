73b67feb7b74ad34f27dc071847ac38f092dfdfb
Erlangga <141829468+PILOTngawur@users.noreply.github.com>
2025-03-07 10:48:44 +0700

    tambah ver

--- EassyKas/src/App.jsx
+++ EassyKas/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
      <h1>EassyKas Project</h1>
+     <h1>v0.0</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
